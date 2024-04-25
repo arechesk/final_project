eadc03bfeabf8cf51f180a953b69d76b73dd0221
Valeriia Chavdar <valera.chavdar@mail.ru>
2024-04-25 18:50:19 +0300

    Исправлены api запросы из раутера Router на сервис билетов Tickets

--- router/app/controllers/tickets_controller.rb
+++ router/app/controllers/tickets_controller.rb
@@ -2,30 +2,46 @@ require 'net/http'
 require "json"
 require "open-uri"
 class TicketsController < ApplicationController
+  skip_before_action :verify_authenticity_token, only: [:update]
+
   def index
     url = "http://app-tickets:8080/tickets"
     data = URI.open(url).read
-    render json: JSON.parse(data)
-    # uri = URI('http://app-tickets:8080/tickets')
-    # response = Net::HTTP.get(uri)
-    # render json: JSON.parse(response)
- end
+    render json: {"wee" => JSON.parse(data)}
+  end
 
- def update
-    uri = URI("http://service.com/tickets/#{params[:id]}")
-    response = Net::HTTP.patch(uri, { status: params[:status] })
+  def update
+    uri = URI("http://app-tickets:8080/tickets/#{ticket_params[:id]}")
+    http = Net::HTTP.new(uri.host, uri.port)
+    request = Net::HTTP::Patch.new(uri.request_uri)
+    body = {
+      "id" => 1,
+      "ticket" => {
+        "id" => ticket_params[:id],
+        "status" => ticket_params[:status]
+      }
+    }
+    request.content_type = "application/json"
+    request.body = body.to_json
+    response = http.request(request)
     render json: JSON.parse(response.body)
- end
+  end
 
- def show
-    uri = URI("http://service.com/tickets/#{params[:id]}")
-    response = Net::HTTP.get(uri)
-    render json: JSON.parse(response)
- end
+  def show
+    url = "http://app-tickets:8080/tickets/#{params[:id]}"
+    data = URI.open(url).read
+    render json: {"wee" => JSON.parse(data)}
+  end
 
- def cost
-    uri = URI("http://service.com/tickets/cost?date=#{params[:date]}&type=#{params[:type]}")
-    response = Net::HTTP.get(uri)
-    render json: JSON.parse(response)
- end
+  def cost
+    url = "http://app-tickets:8080/tickets/cost?date=#{params[:date]}&category=#{params[:category]}"
+    data = URI.open(url).read
+    render json: JSON.parse(data)
+  end
+
+  private
+
+  def ticket_params
+    params.require(:ticket).permit(:status, :id)
+  end
 end
