aea410585a21ab13b238bfdb2a5d025fa763bb0c
Valeriia Chavdar <valera.chavdar@mail.ru>
2024-04-26 21:31:33 +0300

    Fix seeds

--- tickets/db/seeds.rb
+++ tickets/db/seeds.rb
@@ -11,3 +11,31 @@ puts "Creating regular tickets for 09.07.2024"
   print '.'
   STDOUT.flush
 end
+
+puts "Creating vip tickets for 10.07.2024"
+50.times do |i|
+  Ticket.create!(date: "10.07.2024", status: "available", category: "vip")
+  print '.'
+  STDOUT.flush
+end
+
+puts "Creating regular tickets for 10.07.2024"
+150.times do |i|
+  Ticket.create!(date: "10.07.2024", status: "available", category: "regular")
+  print '.'
+  STDOUT.flush
+end
+
+puts "Creating vip tickets for 11.07.2024"
+50.times do |i|
+  Ticket.create!(date: "10.07.2024", status: "available", category: "vip")
+  print '.'
+  STDOUT.flush
+end
+
+puts "Creating regular tickets for 11.07.2024"
+150.times do |i|
+  Ticket.create!(date: "10.07.2024", status: "available", category: "regular")
+  print '.'
+  STDOUT.flush
+end
